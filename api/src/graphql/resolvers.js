3f265c26b55ba04f83335b7f64a538e2522f9bfd
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2021-06-22 23:00:36 -0300

    feat: adicionar merge de resolvers e módulos gql

---
+++ api/src/graphql/resolvers.js
@@ -0,0 +1,10 @@
+import { mergeResolvers } from "@graphql-tools/merge";
+import { loadFilesSync } from "@graphql-tools/load-files";
+import path, { resolve } from "path";
+
+const resolversArray = loadFilesSync(
+  path.join(__dirname, "modules", "**", "resolvers.js")
+);
+const resolvers = mergeResolvers(resolversArray);
+
+export default resolvers;
