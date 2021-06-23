6aeec04f1579fa525a5ffb4af3acd7f66bdfbc20
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2021-06-23 18:01:15 -0300

    feat: adiciona resolver com filtro para estudantes
    Caso filter seja uma string vazia, retornamos todos os resultados.

--- api/src/graphql/modules/students/resolvers.js
+++ api/src/graphql/modules/students/resolvers.js
@@ -3,6 +3,25 @@ import Student from "../../../models/Student";
 export default {
   Query: {
     students: () => Student.find(),
+    fetchStudents: (parent, args, context, info) => {
+      const { filter } = args;
+      const shoudApplyFilters = filter.length > 0;
+
+      if (!shoudApplyFilters) {
+        return Student.find();
+      }
+
+      const regex = new RegExp(filter, "i");
+      const students = Student.aggregate([
+        {
+          $match: {
+            $or: [{ nome: regex }, { cpf: regex }, { email: regex }],
+          },
+        },
+      ]);
+
+      return students;
+    },
   },
   Mutation: {
     createStudent: (_, { data }) => Student.create(data),
