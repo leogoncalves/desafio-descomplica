4a3202c7f1d3580c834e96f5ffea803f8e3b418e
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2021-06-24 16:35:23 -0300

    feat: adicionar tabela para listagem de resultados

---
+++ front/src/components/Table/StudentTable/Table.tsx
@@ -0,0 +1,25 @@
+import { IStudent } from "../../../models/IStudent";
+import { formatterCPF } from "../../../utils.ts/formatters";
+
+export const StudentTable = ({ students }: { students: IStudent[] }) => (
+  <div className="table-container">
+    <table className="table is-fullwidth is-bordered is-striped is-hoverable my-5 mx-auto">
+      <thead>
+        <tr>
+          <td className="has-text-centered has-text-weight-bold">Nome</td>
+          <td className="has-text-centered has-text-weight-bold">E-mail</td>
+          <td className="has-text-centered has-text-weight-bold">CPF</td>
+        </tr>
+      </thead>
+      <tbody>
+        {students.map((student: IStudent) => (
+          <tr key={student.cpf}>
+            <td className="has-text-centered">{student.nome}</td>
+            <td className="has-text-centered">{student.email}</td>
+            <td className="has-text-centered">{formatterCPF(student.cpf)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+);
