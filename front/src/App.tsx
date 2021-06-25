cc5b961ac34b7a4f12cd316a29e1e1b8b1e817a7
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2021-06-24 23:18:18 -0300

    fix: corrigir nome do pacote

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Redirect, Switch } from "react-router";
+import { Route, Redirect, Switch } from "react-router-dom";
 import "./App.scss";
 import { ListStudentsPage } from "./components/pages/ListStudents/ListStudentsPage";
 
